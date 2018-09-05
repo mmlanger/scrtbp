2727e99ca0ba55ae08919452979c1d3e4bb61db3
Martin Langer <martin.langer1@tu-dresden.de>
2018-09-05 18:19:56 +0200

    fix in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 from setuptools import setup, find_packages
 
 
-here = path.abspath(path.dirname(__file__))
+here = path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
