c917144734b367528661f16fd1832e2e08310267
Martin Langer <martin.langer1@tu-dresden.de>
2019-03-20 18:23:07 +0100

    event integration routine with escape exit

--- scrtbp/taylor/escape.py
+++ scrtbp/taylor/escape.py
@@ -87,3 +87,60 @@ def generate_adapt_prec_escape_solver(
         return state, time
 
     return solve_escape
+
+
+def generate_adaptive_escape_event_solver(
+    taylor_params,
+    py_event_function,
+    py_exit_function,
+    order: int = 20,
+    tol_abs: float = 1e-16,
+    tol_rel: float = 1e-16,
+    max_event_steps=10000000,
+    max_steps: int = 1000000000,
+    one_way_mode: bool = True,
+):
+    state_dim = taylor_params[1]
+
+    TaylorExpansion = expansion.generate_taylor_expansion(*taylor_params)
+    Stepper = steppers.generate_adaptive_stepper(TaylorExpansion)
+    StepLimiterProxy = steppers.generate_step_limiter_proxy(Stepper)
+    EventFuncAdapter = expansion.generate_func_adapter(py_event_function)
+    EventObserver = events.generate_event_observer(
+        Stepper, EventFuncAdapter, one_way_mode
+    )
+    ExitFuncAdapter = expansion.generate_func_adapter(py_exit_function)
+    ExitObserver = events.generate_event_observer(
+        Stepper, ExitFuncAdapter, one_way_mode
+    )
+
+    @nb.njit
+    def solve_points(init_cond, n_points, init_t0=0.0):
+        points = np.empty((n_points, state_dim))
+        times = np.empty(n_points)
+
+        stepper = Stepper(init_cond, init_t0, order, tol_abs, tol_rel)
+        limiter = StepLimiterProxy(stepper, max_event_steps, max_steps)
+        event_observer = EventObserver(stepper)
+        exit_observer = ExitObserver(stepper)
+
+        i = 0
+        while stepper.valid():
+            if event_observer.event_detected():
+                times[i] = event_observer.extract_event(points[i])
+                i += 1
+                if i < n_points:
+                    limiter.reset_constraint()
+                else:
+                    break
+
+            if exit_observer.event_detected():
+                return points[:i], times[:i]
+
+            stepper.advance()
+        else:
+            raise exceptions.MaxStepsExceeded
+
+        return points, times
+
+    return solve_points
--- tests/taylor/test_escape.py
+++ tests/taylor/test_escape.py
@@ -120,3 +120,49 @@ def test_escape_box_func():
 
     assert np.allclose(point, escaped_point, rel_tol, abs_tol)
     assert math.isclose(t, escaped_time, rel_tol=rel_tol, abs_tol=abs_tol)
+
+
+def test_event_solver_with_escape():
+    mu = 0.01215
+
+    taylor_params = coeffs.generate_taylor_coeffs(mu)
+
+    escape_char_func = tools.generate_escape_box_func(0.0, 0.85, 0.0, 3.0, 1.3, 2.0)
+    poincare_func = sections.generate_poincare_tools(mu)
+
+    order = 20
+    escape_solve = escape.generate_adaptive_escape_event_solver(
+        taylor_params, poincare_func, escape_char_func, order
+    )
+
+    init_cond = np.array(
+        [
+            0.4318901554339924,
+            0.7691001098124562,
+            0.0,
+            -0.9337249081281812,
+            0.5279878799757336,
+            0.0,
+        ]
+    )
+
+    points, times = escape_solve(init_cond, 1500)
+
+    last_point = np.array(
+        [
+            0.3428822970341644,
+            0.6149339767910579,
+            0.0,
+            -1.1197828725101768,
+            0.5848142640570532,
+            0.0,
+        ]
+    )
+    last_time = 25447.34707126419
+
+    rel_tol = 1e-14
+    abs_tol = 1e-14
+
+    assert points.shape[0] == 1190
+    assert np.allclose(points[-1], last_point, rel_tol, abs_tol)
+    assert math.isclose(times[-1], last_time, rel_tol=rel_tol, abs_tol=abs_tol)
